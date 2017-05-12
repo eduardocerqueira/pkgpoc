b74e78cb40e7d1febefd92fe9acac1b248e2863a
Eduardo Cerqueira <ecerquei@redhat.com>
2017-05-11 23:51:34 -0400

    code format

--- pkgpoc/driver.py
+++ pkgpoc/driver.py
@@ -9,7 +9,8 @@ import logging
 # Setting logging
 logger = logging.getLogger()
 handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s \
+%(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
